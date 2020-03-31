692827d1b70f9f4b5a5d1a35d5c99989b5e0bca3
Yevhenii <yevheniy.miropolets@gmail.com>
2020-03-31 13:42:05 +0300

    Fix default route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
 
-  {path: '*', redirectTo: ''},
+  {path: '**', redirectTo: 'planning'},
 ];
 
 @NgModule({
